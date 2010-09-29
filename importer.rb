061914dadb8a134721f5ebe5d15b55a3eb4ce127
Daniel Hoelbling <tigraine@tigraine.at>
2010-09-29 18:02:42 +0200

    Almost finished blog importer script

--- importer.rb
+++ importer.rb
@@ -1,26 +1,55 @@
 require 'rexml/document'
 require 'date'
+require 'htmlentities'
+require 'fileutils'
 include REXML
 
-file = File.new("wordpress.2010-09-28.xml")
+postsPath = "./_posts"
+wpsource = "wordpress.2010-09-28.xml"
+puts "Migrating Wordpress posts from #{wpsource} to #{postsPath}"
+
+if (!File.exists? wpsource) then
+	puts "Cannot find file '#{wpsource}' please modify wp-source to match your Wordpress export file"
+	exit 1
+end
+
+if (!File.directory? postsPath) then
+	puts "Directory #{postsPath} does not exist. Creating it"
+	FileUtils.mkdir(postsPath)
+end
+
+file = File.new(wpsource)
 doc = REXML::Document.new file
 counter = 0
+skipped = 0
 doc.elements.each("*/channel/item") do |element|
        postname =  element.elements["wp:post_name"].text
-       #puts postname
+       title = element.elements["title"].text
+       postid = element.elements["wp:post_id"].text
+       #Encoding of Post title
+       coder = HTMLEntities.new
+       title =  coder.encode(title, :named)
        postdate = element.elements["wp:post_date"].text
-       if postdate == "0000-00-00 00:00:00" then
+       if postdate == "0000-00-00 00:00:00" or element.elements["wp:post_type"].text != "post" then
 	       next
        end
+       counter += 1
        postdate = DateTime.parse(postdate)
-       #puts postdate
        if postname.nil? or postdate.nil? then 
 	       puts "Post #{element.elements["wp:post_id"].text} is incomplete"
+	       skipped += 1
        end
 
        postdate = postdate.strftime("%Y-%m-%d")
 
-       targetFile = File.open("./posts/#{postdate}-#{postname}.html", "w")
+       targetFile = File.open("./_posts/#{postdate}-#{postname}.html", "w:ASCII-8BIT")
+       targetFile.puts("---")
+       targetFile.puts("layout: post")
+       targetFile.puts("title: \"#{title}\"")
+       targetFile.puts("id: #{postid}")
+       targetFile.puts("---")
        targetFile.write(element.elements["content:encoded"].text)
        targetFile.close
 end
+
+puts "Finished migration of #{counter} posts. Skipped #{skipped} posts"
